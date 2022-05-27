2ed968006f4853c936c527631a2e186cd514b3c8
kuns019 <56548175+kuns019@users.noreply.github.com>
2022-05-27 12:52:33 +0700

    Update path

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -2,15 +2,34 @@ import * as React from "react";
 import { Grid } from "@mui/material";
 import { useLogin } from "../contexts/LoginContext";
 import { useEffect } from "react";
+import { useQuery, gql } from "@apollo/client";
+import axios from 'axios';
+import { useState } from "react";
+
+const USERS_QUERY = gql`
+  {
+    users{
+      googleId
+    }
+  }
+`;
 
 function Login() {
   const {loginAsAdmin, loginAsUser} = useLogin()
+  const { data, loading, error } = useQuery(USERS_QUERY);
+  const [user, setUser] = useState([]);
 
   useEffect(() => {
-    
+    axios.get(`https://localhost:4000/login`)
+      .then(res => {
+        const persons = res.data;
+        setUser({ persons });
+      })
   },
   [loginAsUser])
 
+  if (loading) return "Loading...";
+  if (error) return <pre>{error.message}</pre>
   return (
     <Grid
       container
